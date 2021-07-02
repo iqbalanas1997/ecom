c23853157319f1fe4c02a4f06fdb8bf1bcce8d08
IqbalAnas <iqbalana99.ia@gmail.com>
2021-07-02 04:23:47 -0700

    Profile Page: profile photo, Heading, email, Upgrade button

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,7 @@
+import 'package:ecom/profilewidget.dart';
+import 'package:ecom/user_preferences.dart';
+import 'package:ecom/users.dart';
+import 'package:ecom/widget/buttonwidget.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -8,15 +12,46 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
     return Scaffold(
-     
       body: ListView(
         physics: BouncingScrollPhysics(),
         children: [
-          
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          buildName(user),
+          SizedBox(
+            height: 24,
+          ),
+          Center(child: buildUpgradeBtn()),
         ],
-        
       ),
     );
   }
+
+  Widget buildName(Users user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            user.email,
+            style: TextStyle(fontSize: 15, color: Colors.grey),
+          )
+        ],
+      );
+
+  Widget buildUpgradeBtn() => ButtonWidget(
+        text: 'Upgrade to pro',
+        onClicked: () {},
+      );
 }
--- lib/user_preferences.dart
+++ lib/user_preferences.dart
@@ -1,1 +1,12 @@
-class UserPreferences {}
+import 'package:ecom/users.dart';
+import 'package:flutter/material.dart';
+
+class UserPreferences {
+  static const myUser = Users(
+    imagePath: 'assets/iqbal.jpg',
+    name: 'Iqbal Anas',
+    email: 'iqblanas99.ia@gmail.com',
+    about: 'Hybrid Mobile Fluuter Application Developer',
+    isDarkMode: false,
+  );
+}
